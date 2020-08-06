efcb781f70ee8d1574148d5c51eb7e24cd3974f8
Travis Frazier <travisfrazier@traviss-mbp.lan>
2020-08-05 19:28:53 -0700

    updates

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,18 @@
 <template>
   <footer class="footer">
-    <div class="container">
+    <div class="container footer-container">
       <span v-html="settings.footer" />
-      <div class="social-links">
-        <a target="_blank" href="https://instagram.com/miserable.media"
+      <span class="social-links">
+        <a
+          class="instagram"
+          target="_blank"
+          href="https://instagram.com/miserable.media"
           ><span>Instagram</span></a
         >
         <a target="_blank" href="https://www.youtube.com/c/TheMiserableCo/"
           ><span>YouTube</span></a
         >
-      </div>
+      </span>
     </div>
   </footer>
 </template>
@@ -26,10 +29,23 @@ export default {
 
 <style scoped>
 .footer {
+  margin-top: 3rem;
   font-size: 0.8rem;
   padding: 6rem 0;
+  background-color: #d9bc7a;
 }
-.social-links span:first-of-type {
-  margin-right: 10px;
+span {
+  padding: 1rem;
+}
+@media only screen and (max-width: 768px) {
+  .footer-container {
+    display: grid;
+  }
+  span {
+    padding: 0;
+  }
+  .instagram {
+    margin-right: 1rem;
+  }
 }
 </style>
